b6b61f30aaae4c0e04b9f16acc1e791286eb7ea4
Jonas Hauser <hauser@hasch.io>
2022-04-08 23:39:43 +0200

    Added missing method

--- src/DependencyInjection/GoogleAuthenticatorExtension.php
+++ src/DependencyInjection/GoogleAuthenticatorExtension.php
@@ -23,4 +23,10 @@ use Symfony\Component\DependencyInjection\Loader;
  */
 class GoogleAuthenticatorExtension extends Extension
 {
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+
+        $config = $this->processConfiguration($configuration, $configs);
+    }
 }
